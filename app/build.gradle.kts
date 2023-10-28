a13b87ef07249a38c3fcfc90bacd5039fa885b37
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-10-29 02:36:52 +0300

    рефакторинг замечаний

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.androidsecretsprint"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.androidsecretsprint"
@@ -44,6 +44,7 @@ dependencies {
     implementation ("androidx.fragment:fragment-ktx:1.6.1")
     implementation ("androidx.databinding:databinding-runtime:8.1.2")
     implementation ("com.google.code.gson:gson:2.9.1")
+    implementation("androidx.activity:activity-ktx:1.8.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/androidsecretsprint/MainActivity.kt
+++ app/src/main/java/com/example/androidsecretsprint/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.androidsecretsprint
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.service.voice.VoiceInteractionSession.ActivityId
 
 class MainActivity : AppCompatActivity() {
-    private var binding = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(binding)
+        setContentView(R.layout.activity_main)
     }
 }

f43515a5c9ece22630e640f612a0af26b4c3923b
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-11-01 14:28:59 +0300

    RA-05 Настроил базовую навигацию по кнопкам категории и избранное

--- app/src/main/java/com/example/androidsecretsprint/FavoritesFragment.kt
+++ app/src/main/java/com/example/androidsecretsprint/FavoritesFragment.kt
@@ -6,20 +6,20 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import com.example.androidsecretsprint.databinding.FragmentListCategoriesBinding
+import com.example.androidsecretsprint.databinding.FragmentFavoritesBinding
 
-class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
-    private lateinit var binding: FragmentListCategoriesBinding
+class FavoritesFragment : Fragment(R.layout.fragment_favorites) {
+    private lateinit var binding: FragmentFavoritesBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
+        binding = FragmentFavoritesBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        val textView = view?.findViewById<TextView>(R.id.fragmentCategoriesText)
+        val textView = view?.findViewById<TextView>(R.id.favoritesText)
         textView?.text
     }
 }

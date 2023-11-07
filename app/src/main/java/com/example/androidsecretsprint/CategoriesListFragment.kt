42ad8b343403897879aa902e3325d6070585d71a
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-11-07 19:09:46 +0300

    заменил парсинг json'a на список

--- app/src/main/java/com/example/androidsecretsprint/CategoriesListFragment.kt
+++ app/src/main/java/com/example/androidsecretsprint/CategoriesListFragment.kt
@@ -1,52 +1,56 @@
 package com.example.androidsecretsprint
 
-import android.content.res.AssetManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidsecretsprint.databinding.FragmentListCategoriesBinding
-import org.json.JSONArray
-import org.json.JSONObject
-import java.io.InputStream
 
 class CategoriesListFragment : Fragment(R.layout.fragment_list_categories) {
     private lateinit var binding: FragmentListCategoriesBinding
-    private val assets: AssetManager = requireContext().assets
+
+    private val categories = listOf<Category>(
+        Category(
+            0,
+            "Бургеры",
+            "Рецепты всех популярных видов бургеров",
+            "burger.png",
+        ),
+        Category(
+            1,
+            "Десерты",
+            "Самые вкусные рецепты десертов специально для вас",
+            "dessert.png",
+        ),
+        Category(
+            2,
+            "Пицца",
+            "Пицца на любой вкус и цвет. Лучшая подборка для тебя",
+            "pizza.png",
+        ),
+        Category(
+            3,
+            "Рыба",
+            "Печеная, жареная, сушеная, любая рыба на твой вкус",
+            "fish.png",
+        ),
+        Category(
+            4,
+            "Супы",
+            "От классики до экзотики: мир в одной тарелке",
+            "soup.png",
+        ),
+        Category(
+            5,
+            "Салаты",
+            "Хрустящий калейдоскоп под соусом вдохновения",
+            "salad.png",
+        ),
+    )
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentListCategoriesBinding.inflate(inflater, container, false)
         return binding.root
     }
-
-    fun readJSONFromAsset(assetManager: AssetManager, fileName: String): String {
-        val inputStream: InputStream = assetManager.open(fileName)
-        val size: Int = inputStream.available()
-        val buffer = ByteArray(size)
-        inputStream.read(buffer)
-        inputStream.close()
-        return String(buffer)
-    }
-
-    // Чтение данных из JSON и создание списка объектов типа Category
-    fun createCategoryList(assetManager: AssetManager, fileName: String): List<Category> {
-        val jsonString = readJSONFromAsset(assetManager, fileName)
-        val jsonArray = JSONArray(jsonString)
-        val categories = mutableListOf<Category>()
-
-        for (i in 0 until jsonArray.length()) {
-            val item: JSONObject = jsonArray.getJSONObject(i)
-            val imageUrl = item.getString("imageUrl").substringAfterLast("/")
-            val category = Category(
-                item.getInt("id"), item.getString("title"),
-                item.getString("description"), item.getString(imageUrl)
-            )
-            categories.add(category)
-        }
-
-        return categories
-    }
-
-    val categories = createCategoryList(assets, "category.json")
 }

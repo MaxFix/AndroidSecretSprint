eccda92a0f3cfda71e6218495cc7f8a2f651cc75
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-12-06 22:50:46 +0300

    RA-11 изменил название слоя рецепта

--- app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
+++ app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
@@ -7,7 +7,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.androidsecretsprint.databinding.RecipeBinding
 
-class RecipeFragment : Fragment(R.layout.recipe) {
+class RecipeFragment : Fragment(R.layout.fragment_recipe) {
     private lateinit var binding: RecipeBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {

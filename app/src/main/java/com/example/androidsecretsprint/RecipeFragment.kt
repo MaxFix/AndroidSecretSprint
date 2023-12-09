7d5a47b151bf5adb4cbcfe95f7aad1676aa60396
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-12-10 01:15:52 +0300

    RA-13 Реализовал в шапке изображение и название рецепта, стилизовал согласно макету

--- app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
+++ app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
@@ -1,12 +1,16 @@
 package com.example.androidsecretsprint
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.androidsecretsprint.databinding.FragmentRecipeBinding
+import java.io.InputStream
 
 class RecipeFragment : Fragment(R.layout.fragment_recipe) {
     private lateinit var binding: FragmentRecipeBinding
@@ -18,16 +22,25 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //val recipeDescription: TextView = binding.recipeDescription
+        val recipeHeaderTitle: TextView = binding.recipeHeaderText
+        val recipeHeaderImage: ImageView = binding.recipeHeaderImg
+        val fragment = context
 
         if (Build.VERSION.SDK_INT == Build.VERSION_CODES.TIRAMISU) {
             val recipeParcelable: Recipe? = arguments?.getParcelable("recipe", Recipe::class.java)
             recipeParcelable?.let { recipe: Recipe ->
-                //recipeDescription.text = recipe.title
+                val inputStream: InputStream? = recipeParcelable.imageUrl.let { fragment?.assets?.open(it) }
+                val drawable = Drawable.createFromStream(inputStream, null)
+                recipeHeaderImage.setImageDrawable(drawable)
+                recipeHeaderTitle.text = recipeParcelable.title
+
             }
         } else {
             val recipeParcelable = arguments?.getParcelable<Recipe>("recipe")
-            //recipeDescription.text = recipeParcelable?.title
+            val inputStream: InputStream? = recipeParcelable?.imageUrl?.let { fragment?.assets?.open(it) }
+            val drawable = Drawable.createFromStream(inputStream, null)
+            recipeHeaderImage.setImageDrawable(drawable)
+            recipeHeaderTitle.text = recipeParcelable?.title
         }
     }
 }

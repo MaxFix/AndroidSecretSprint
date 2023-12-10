a0e065adc8da5e347c1ad7b4d544a926b0d01a1c
ma.spiridonov <ma.spiridonov@corp.vk.com>
2023-12-10 23:19:31 +0300

    RA-13 Сделал разметку фрагмента рецепта

--- app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
+++ app/src/main/java/com/example/androidsecretsprint/RecipeFragment.kt
@@ -1,13 +1,16 @@
 package com.example.androidsecretsprint
 
+import android.graphics.drawable.Drawable
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.androidsecretsprint.databinding.FragmentRecipeBinding
+import java.io.InputStream
 
 class RecipeFragment : Fragment(R.layout.fragment_recipe) {
     private lateinit var binding: FragmentRecipeBinding
@@ -19,8 +22,12 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val recipeDescription: TextView = binding.recipeDescription
+        val recipeDescription: TextView = binding.recipeHeaderText
+        val recipeHeaderImage: ImageView = binding.recipeHeaderImg
+
+        val fragment = context
         var recipeParcelable: Recipe? = arguments?.getParcelable(Constants.ARG_RECIPE)
+
         recipeParcelable = if (Build.VERSION.SDK_INT == Build.VERSION_CODES.TIRAMISU) {
             arguments?.getParcelable(Constants.ARG_RECIPE, Recipe::class.java)
         } else {
@@ -29,6 +36,9 @@ class RecipeFragment : Fragment(R.layout.fragment_recipe) {
 
         recipeParcelable?.let { recipe ->
             recipeDescription.text = recipe.title
+            val inputStream: InputStream? = recipeParcelable.imageUrl.let { fragment?.assets?.open(it) }
+            val drawable = Drawable.createFromStream(inputStream, null)
+            recipeHeaderImage.setImageDrawable(drawable)
         }
     }
 }

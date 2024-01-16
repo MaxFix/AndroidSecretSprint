8f779dedfddd1ca7856f4ce7feb805ce45b53de7
ma.spiridonov <ma.spiridonov@corp.vk.com>
2024-01-17 00:53:25 +0300

    RA-20 Перенес класс стейта в VM, добавил объект и изображение рецепта в свойства стейта, убрал ingredientsCount

--- app/src/main/java/com/example/androidsecretsprint/ui/recipies/recipe/RecipeUiState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.androidsecretsprint.ui.recipies.recipe
-
-data class RecipeUiState(
-    val favoriteRecipe: Boolean,
-    val portionsCount: Int,
-    val ingredientsCount: Int
-)

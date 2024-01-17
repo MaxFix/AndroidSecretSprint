bf8c43c1585ac598ed5441e32416dcbe7d6d9f50
ma.spiridonov <ma.spiridonov@corp.vk.com>
2024-01-17 23:42:35 +0300

    RA-22 Поменял тип ЛД, создал экземпляр класса с пустым конструктором, удалил лишнее, рефакторинг

--- app/src/main/java/com/example/androidsecretsprint/ui/recipies/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/androidsecretsprint/ui/recipies/recipe/RecipeViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.androidsecretsprint.ui.recipies.recipe
 
 import android.graphics.drawable.Drawable
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.androidsecretsprint.model.Recipe
@@ -17,21 +16,5 @@ class RecipeViewModel(
     val favoriteRecipe: Boolean = false,
     val portionsCount: Int? = null,
 ) : ViewModel() {
-    private val _liveData: LiveData<String>? = null
-
-    val liveData: LiveData<String>?
-        get() = _liveData
-
-    val currentRecipe: LiveData<Recipe> by lazy {
-        MutableLiveData<Recipe>()
-    }
-    val currentRecipeDrawable: LiveData<Drawable> by lazy {
-        MutableLiveData<Drawable>()
-    }
-    val currentFavoriteRecipe: LiveData<Boolean> by lazy {
-        MutableLiveData<Boolean>()
-    }
-    val currentPortionsCount: LiveData<Int> by lazy {
-        MutableLiveData<Int>()
-    }
+    private val _liveData: MutableLiveData<RecipeViewModel> = MutableLiveData()
 }

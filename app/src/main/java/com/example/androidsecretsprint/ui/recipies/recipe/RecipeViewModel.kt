768d5f6b32cdae4aa1f5c3c4db8c6fb136fed505
ma.spiridonov <ma.spiridonov@corp.vk.com>
2024-01-17 20:45:22 +0300

    RA-21 Создать LiveData для хранения состояния экрана

--- app/src/main/java/com/example/androidsecretsprint/ui/recipies/recipe/RecipeViewModel.kt
+++ app/src/main/java/com/example/androidsecretsprint/ui/recipies/recipe/RecipeViewModel.kt
@@ -1,6 +1,8 @@
 package com.example.androidsecretsprint.ui.recipies.recipe
 
 import android.graphics.drawable.Drawable
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.androidsecretsprint.model.Recipe
 
@@ -14,5 +16,22 @@ class RecipeViewModel(
     val recipeDrawable: Drawable? = null,
     val favoriteRecipe: Boolean = false,
     val portionsCount: Int? = null,
-    val ingredientsCount: Int? = null,
-) : ViewModel()
+) : ViewModel() {
+    private val _liveData: LiveData<String>? = null
+
+    val liveData: LiveData<String>?
+        get() = _liveData
+
+    val currentRecipe: LiveData<Recipe> by lazy {
+        MutableLiveData<Recipe>()
+    }
+    val currentRecipeDrawable: LiveData<Drawable> by lazy {
+        MutableLiveData<Drawable>()
+    }
+    val currentFavoriteRecipe: LiveData<Boolean> by lazy {
+        MutableLiveData<Boolean>()
+    }
+    val currentPortionsCount: LiveData<Int> by lazy {
+        MutableLiveData<Int>()
+    }
+}
